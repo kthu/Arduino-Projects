6b798c6be425236b79b27a4b3d6cdbe0772c1172
Kjetil Thuen <Kjetil.Thuen@gmail.com>
2012-02-26 17:21:29 +0100

    Some code cleanups

--- PotentialLeds/PotentialLeds.ino
+++ PotentialLeds/PotentialLeds.ino
@@ -24,48 +24,43 @@ int alertLed = 0;
 int val = 0; 
 
 void setup() {
-  for (int i=0; i < numberOfLeds; i++) {
-    pinMode(ledPins[i], OUTPUT);
-  }
-  
-  Serial.begin(9600);
+   for (int i=0; i < numberOfLeds; i++) {
+      pinMode(ledPins[i], OUTPUT);
+   }
+
+   Serial.begin(9600);
 }
 
 void loop() {
-  val = analogRead(potPin);    // read the value from the sensor
-  Serial.println(val);   
-  
-  alert = (val > (POTMAX));
-    
-  if (alertLed == numberOfLeds) {
-      direction = DECREASE;
-  }
-  
-  if (alertLed == -1) {
-      direction = INCREASE;
-  }
-  
-  alertLed = alertLed + direction;
+   val = analogRead(potPin);    // read the value from the sensor
+   Serial.println(val);   
 
-  if (alert) {
-    for (int i=0; i < numberOfLeds); i++) {
-      if (i == alertLed) {
-           digitalWrite(ledPins[i], HIGH);    
-      } else {
-           digitalWrite(ledPins[i], LOW);    
-      }
-    }
-    
+   alert = (val > (POTMAX));
 
-  } else {
-    for (int i=0; i < numberOfLeds; i++) {    
-      if (val > (highestLedPotVal / (i + 1)) ) {  
-        digitalWrite(ledPins[i], HIGH);
-      } else {
-        digitalWrite(ledPins[i], LOW);      
+   if (alert) {
+      if (alertLed == numberOfLeds-1) {
+         direction = DECREASE;
+      } else if (alertLed == 0) {
+         direction = INCREASE;
       }
-    }
-  }  
-    
-  delay(100);          // not so fast!
+
+      alertLed = alertLed + direction;
+      for (int i=0; i < numberOfLeds; i++) {
+         if (i == alertLed) {
+            digitalWrite(ledPins[i], HIGH);    
+         } else {
+            digitalWrite(ledPins[i], LOW);    
+         }
+      }
+   } else {
+      for (int i=0; i < numberOfLeds; i++) {    
+         if (val > (highestLedPotVal / (i + 1)) ) {  
+            digitalWrite(ledPins[i], HIGH);
+         } else {
+            digitalWrite(ledPins[i], LOW);      
+         }
+      }
+   }  
+
+   delay(100);          // not so fast!
 }
